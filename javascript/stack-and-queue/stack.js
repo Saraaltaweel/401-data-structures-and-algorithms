b5f03068d721a0349f0e383882e6db02e2947072
Saraaltaweel <s.bata97.st@gmail.com>
2021-07-08 18:50:25 +0300

    ch10

---
+++ javascript/stack-and-queue/stack.js
@@ -0,0 +1,44 @@
+'use strict';
+
+class Node{
+    constructor(val,next){
+        this.val=val;
+        this.next=next;
+
+    }
+
+    pointer(node){
+        this.next=node;
+    }
+
+}
+
+class Stack{
+    constructor(){
+        this.top=null;
+    }
+
+    isEmpty(){
+        return this.top === null;
+    }
+
+    push(val){
+        let newTop= new Node(val);
+        newTop.pointer=this.top;
+        this.top=newTop;
+      return this;  
+    }
+
+    pop(){
+        let newPop=this.top;
+        this.top=this.top.next;
+        newPop.pointer=null;
+        return this;  
+
+    }
+
+    peek(){
+        return this.top.val;
+    }
+}
+module.exports= Stack , Node;
---
+++ javascript/stack-and-queue/stack.test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const Stack=require('./stack');
+
+describe('stack',()=>{
+    let stack;
+    beforeEach(()=>{
+        stack=new Stack();
+    });
+
+    it('return null on empty Stack', () => {
+        expect(stack.peek()).toBeNull(true);
+      });
+
+    it('Creates an instance of Stack', () => {
+        expect(stack instanceof Stack).toBeTruthy();
+      });
+    
+      describe('peek', () => {
+       
+        it('return last item added on the Stack', () => {
+          stack.push(1);
+          expect(stack.peek()).toEqual(1);
+          stack.push(2);
+          expect(stack.peek()).toEqual(2);
+        });
+        it('does not alter the stack', () => {
+          stack.push(1);
+          expect(stack.peek()).toEqual(1);
+      
+        });
+       
+      });
+    //   describe('push', () => {
+    //     it('add items to the top of the stack', () => {
+    //       stack.push(1);
+    //       stack.push(2);
+    //       stack.push(3);
+    //       expect(stack.peek()).toEqual(3);
+    //     });
+    //   });
+      describe('pop', () => {
+        it('add items to the top of the stack', () => {
+          stack.push(1);
+          stack.push(2);
+          stack.push(3);
+          expect(stack.pop()).toEqual(3);
+          expect(stack.pop()).toEqual(2);
+          expect(stack.pop()).toEqual(1);
+          expect(stack.peek()).toBeNull();
+        });
+      });
+    });
